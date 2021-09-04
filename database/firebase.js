76c712907648f513a685b95adf6927b542663e1f
Axel <74220980+Axelcv24@users.noreply.github.com>
2021-09-04 11:00:57 -0600

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firebase-storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm5eU8v83fbRxGQa_x-n5MTE6gt56ebjU",
-  authDomain: "react-native-firebase-a1480.firebaseapp.com",
-  projectId: "react-native-firebase-a1480",
-  storageBucket: "react-native-firebase-a1480.appspot.com",
-  messagingSenderId: "45479696230",
-  appId: "1:45479696230:web:4d1d76662bfe8410b05aa7"
+  apiKey: "******",
+  authDomain: "*****",
+  projectId: "*******",
+  storageBucket: "*******",
+  messagingSenderId: "*******",
+  appId: "******"
 };
 
 // Initialize Firebase
